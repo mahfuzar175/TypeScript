75cfbd14b47cd2e1b9778f674f29fc7df0d70e15
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-07 23:25:30 +0600

    create a first typescript program

---
+++ first-program/index.js
@@ -0,0 +1,17 @@
+var useName = "Mahfuzar Rahman";
+var age = 25;
+console.log(useName);
+console.log(age);
+//original way 
+var Uname;
+Uname = "tarek";
+console.log(Uname);
+// 1st Function
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(23, 54);
+// ==========run ts file=========
+// tsc index.ts -> index.js -> node index.js
+// ==========for auto comple===========
+// tsc index.ts --watch
---
+++ first-program/index.ts
@@ -0,0 +1,28 @@
+let useName = "Mahfuzar Rahman";
+let age = 25;
+console.log(useName);
+console.log(age);
+
+//original way 
+let Uname: string;
+Uname = "tarek";
+console.log(Uname);
+
+
+// 1st Function
+function addNumber(num1: number, num2: number){
+    console.log(num1 + num2);
+}
+addNumber(23, 54);
+
+
+
+// ==========run ts file=========
+// tsc index.ts -> index.js -> node index.js
+
+// ==========for auto comple===========
+// tsc index.ts --watch
+
+
+
+
