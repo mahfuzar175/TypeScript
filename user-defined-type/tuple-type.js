2ba191ec338274baf1c34054ae83cce39b62d881
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-08 16:45:34 +0600

    practices tuple type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,6 @@
+// mixed data-key, value
+var user;
+user = [101, "tarek"];
+user.push(102, "sumu");
+console.log(user);
+console.log(user[0]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mixed data-key, value
+let user: [number, string];
+user = [101, "tarek"];
+
+user.push(102, "sumu");
+
+console.log(user);
+console.log(user[0]);
